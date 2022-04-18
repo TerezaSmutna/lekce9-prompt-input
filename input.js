fa127b5fe6b007ff1b33dba593a82efc8642af22
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-04-18 22:14:27 +0200

    Lesson 9 - homework

--- input.js
+++ input.js
@@ -1,13 +1,13 @@
-
-  let prvniCislo = document.querySelector('input[name="cislo1"]').value;
-  let druheCislo = document.querySelector('input[name="cislo2"]').value;
-
-function vypisSoucet (prvniCislo, druheCislo) {
+function vypisSoucet(prvniCislo, druheCislo) {
   let soucetCisel = prvniCislo + druheCislo;
-  document.querySelector('#vysledek > span').innerHTML = soucetCisel;
   return soucetCisel;
 }
 
-document.querySelectorAll('input').forEach((element) => {
-  element.addEventListener('change', vypisSoucet);
+const vysledek = document.querySelector('#vysledek > span');
+
+document.getElementById('tlacitko').addEventListener('click', () => {
+  let prvniCislo = parseInt(document.querySelector('input[name="cislo1"]').value);
+  let druheCislo = parseInt(document.querySelector('input[name="cislo2"]').value);
+  vysledek.innerHTML = vypisSoucet(prvniCislo, druheCislo);
 });
+
--- rodne-cislo.js
+++ rodne-cislo.js
@@ -1,10 +1,9 @@
-let rodneCislo = document.querySelector('input[name="rodne-cislo"]');
-
-function zkontroluj() {
-  if (zkontrolujRodneCislo(rodneCislo) === true) {
-    return true;
+function zkontroluj(rodneCislo) {
+  let rodneCislo = document.querySelector('input[name="rodne-cislo"]').value;
+  if (zkontrolujRodneCislo(rodneCislo)) {
+    alert('Správně zadané rodné číslo.');
   } else {
-    alert('Chybně zadaná informace.');
+    alert('Chybně zadané rodné číslo.');
   }
 }
 
@@ -12,39 +11,21 @@ function zkontrolujRodneCislo(rodneCislo) {
   let year = parseInt(rodneCislo.substr(0, 2), 10);
   let month = parseInt(rodneCislo.substr(2, 2), 10);
   let day = parseInt(rodneCislo.substr(4, 2), 10);
-  let lomitko = (rodneCislo.substr(6, 1), 10);
+  let lomitko = rodneCislo.substr(6, 1);
   let bezLomitka = rodneCislo.replace('/', '');
   let ext = parseInt(rodneCislo.substr(7, 4), 10);
+  let ext_delka = ext.length;
   let zbytek = parseInt(bezLomitka) % 11;
 
-  if ((year > 53) && (ext === parseInt(rodneCislo.substr(7, 4), 10)) && (zbytek === 0)) {
-    true;
-  } else {
-    return false
-  };
-
-  if ((year <= 53) && ((ext === parseInt(rodneCislo.substr(7, 4), 10)) || (ext === parseInt(rodneCislo.substr(7, 3), 10)))) {
-    true;
-  } else {
-    return false;
-  };
-
-  if ((month > 0 && month < 13) || (month < 50 && month < 63)) {
-    true;
-  } else {
-    return false;
-  };
-
-  if (day > 0 && day < 32) {
-    true;
-  } else {
-    return false;
-  };
-
-  if (!(lomitko = "/")) {
-    return false;
-  }
+  if ((year > 53 && zbytek === 0) || (year <= 53 && ((ext_delka === 4 && zbytek === 0)|| ext_delka === 3))) {
+    if ((month > 0 && month < 13) || (month > 50 && month < 63)) {
+      if (day > 0 && day < 32) {
+        if (lomitko === "/") {
+          return true;
+        }
+      } 
+    }
+  } 
+  return false;
 }
-
-//rodneCislo.addEventListener('change', zkontrolujRodneCislo);
 
